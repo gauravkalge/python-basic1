6ea29f9f7604bcd4ca5810c90ba10f7b48ff43e8
Gaurav Kalge <42073937+kalge@users.noreply.github.com>
2022-03-04 12:19:08 +0530

    initial commit

---
+++ p.py
@@ -0,0 +1,13 @@
+class Laptop:
+    dis=10
+    gst=9
+    def __init__(self,p,b):
+        print('tnside the laptop con')
+        self.b=None
+        self._ram_capacity=4
+        self._processor='15'
+        self._price=54000
+        b='Apple'
+        pro='AMD'
+
+Laptop 
